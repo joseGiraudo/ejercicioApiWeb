85f869d78fc479f17ad064c4ac7d8fbcbf5470e3
joseGiraudo <josegiraudo7@gmail.com>
2024-12-09 18:39:39 -0300

    .

--- Solution1/ClassLibrary/Services/BookService.cs
+++ Solution1/ClassLibrary/Services/BookService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ClassLibrary.DAOs.Interfaces;
+using ClassLibrary.models;
+using ClassLibrary.Response;
+using ClassLibrary.Services.Interfaz;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,47 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary.Services
 {
-    public class BookService
+    public class BookService : IBookService
     {
+        private readonly IBookDAO _bookDAO;
+        public BookService(IBookDAO bookDAO)
+        {
+            _bookDAO = bookDAO;
+        }
+
+        public async Task<ApiResponse<Book>> Create(Book book)
+        {
+            var response = new ApiResponse<Book>();
+
+            var affRows = _bookDAO.Create(book);
+
+            if (affRows > 0)
+            {
+                response.Data = book;
+                return response;
+            }
+            response.SetError("No se pudo crear el usuario ", System.Net.HttpStatusCode.InternalServerError);
+            return response;
+        }
+
+        public Task<ApiResponse<string>> DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<List<Book>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<Book>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<Book>> Update(int id, Book book)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
